612d143d9693605195d84669e3f08065aa80c225
vsolianin <vsolianin@gmail.com>
2023-05-13 14:43:07 +0200

    questions-react-first-commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Questions from './Questions';
+import data from './data';
+import { useState } from 'react';
+
+
+const App = () => {
+  const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+  
+  const toggleQuestion = (id) => {
+    console.log(id, 'id');
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  }
+
+  return (
+    <main>
+      <Questions 
+        questions={questions}
+        activeId={activeId} 
+        toggleQuestion={toggleQuestion}
+      />
+    </main>
+  );
+};
+export default App;
